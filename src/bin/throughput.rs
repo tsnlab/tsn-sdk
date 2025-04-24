96bbc830e39ba0e5770c061d61dbf17a80161465
Junho Lee <junho@tsnlab.com>
2025-04-24 17:21:47 +0900

    Revert stat thread spawn timing

--- src/bin/throughput.rs
+++ src/bin/throughput.rs
@@ -165,8 +165,6 @@ fn do_server(iface_name: String) {
         }
     });
 
-    let mut received_first_data = false;
-
     while unsafe { RUNNING } {
         let mut packet = [0u8; 1514];
         let packet_size;
@@ -204,6 +202,9 @@ fn do_server(iface_name: String) {
                     TEST_RUNNING = true;
                 }
 
+                // Make thread for statistics
+                thread::spawn(stats_worker);
+
                 let mut perf_buffer = vec![0; 8];
                 let mut eth_buffer = vec![0; 14 + 8];
 
@@ -222,11 +223,6 @@ fn do_server(iface_name: String) {
                 }
             }
             PerfOpFieldValues::Data => {
-                if !received_first_data {
-                    // Make thread for statistics
-                    received_first_data = true;
-                    thread::spawn(stats_worker);
-                }
                 unsafe {
                     STATS.last_id = perf_pkt.get_id();
                     STATS.pkt_count += 1;
@@ -237,7 +233,6 @@ fn do_server(iface_name: String) {
                 println!("Received ReqEnd");
 
                 unsafe { TEST_RUNNING = false }
-                received_first_data = false;
 
                 let mut perf_buffer = vec![0; 8];
                 let mut eth_buffer = vec![0; 14 + 8];
